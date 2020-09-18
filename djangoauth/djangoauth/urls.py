8f50172c55f8cb3e0e505b2cd830c9bd2fe6a5c9
letsblogcontent <letsblogcontent@gmail.com>
2020-09-18 16:56:25 +0530

    authentication

--- djangoauth/djangoauth/urls.py
+++ djangoauth/djangoauth/urls.py
@@ -1,4 +1,4 @@
-"""djangoauthentication URL Configuration
+"""djangoauth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -8,19 +8,14 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name=python'home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from django.conf.urls import url
 from django.views.generic.base import TemplateView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +25,4 @@ urlpatterns += [
     path('', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'))
 ]
+
